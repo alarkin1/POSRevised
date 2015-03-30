75e23be3d8cc7005ec2b6205dca55dee47ada9c2
Alex <Alex@MajorFox>
2015-03-30 13:24:25 -0500

    Added two custom exception classes InvalidCustomerIdException and InvalidNameException.  Updated All of the output strategy objects with error handling.

---
+++ src/newpossystem/InvalidNameException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpossystem;
+
+/**
+ *
+ * @author Alex
+ */
+public class InvalidNameException extends RuntimeException{
+
+    public InvalidNameException(String message) {
+        super(message);
+    }
+    
+}
--- src/newpossystem/RecordedCustomer.java
+++ src/newpossystem/RecordedCustomer.java
@@ -9,9 +9,11 @@ package newpossystem;
  *
  * @author Alex
  */
-public class RecordedCustomer implements DatabaseCustomerStrategy{
+public class RecordedCustomer implements DatabaseCustomerStrategy {
+
     String name;
     String customerId;
+
     public RecordedCustomer(String name, String customerId) {
         this.name = name;
         this.customerId = customerId;
@@ -26,5 +28,21 @@ public class RecordedCustomer implements DatabaseCustomerStrategy{
     public String getCustomerId() {
         return customerId;
     }
-    
+
+    public void setName(String name) throws RuntimeException {
+        if (name != null || name.isEmpty() == false || (name.length() > 0 && name.length() < 20)) {
+            this.name = name;
+        } else {
+            throw new InvalidNameException("Invalid name has been passed in.  Rules: Not null, Not empty string, Longer than 0 characters, Less than 20 characters");
+        }
+    }
+
+    public void setCustomerId(String customerId) {
+        if (customerId != null || customerId.isEmpty() == false || (customerId.length() > 0 && customerId.length() <= 7)) {
+            this.customerId = customerId;
+        }else {
+            throw new InvalidCustomerIdException("Invalid name has been passed in.  Rules: Not null, Not empty string, Longer than 0 characters, Less than or equal to 7 characters");
+        }
+    }
+
 }
